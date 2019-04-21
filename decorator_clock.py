75b2a087c38b8c760511d4a3228089b19f903d80
Syntactical01 <46199814+Syntactical01@users.noreply.github.com>
2019-04-20 21:44:46 -0500

    Create decorator_clock.py

---
+++ decorator_clock.py
@@ -0,0 +1,29 @@
+# Page 205
+import time
+import functools
+
+def clock(func):
+    @functools.wraps(func)
+    def clocked(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        elapsed = time.time() - t0
+        name = func.__name__
+        arg_list = []
+        if args:
+            arg_list.append(', '.join(repr(arg) for arg in args))
+        if kwargs:
+            pairs = ['{}={}'.format(k, w) for k, w in sorted(kwargs.items())]
+            arg_list.append(','.join(pairs))
+        arg_str = ', '.join(arg_list)
+        print('[%0.8fs] %s(%s) -> %r ' % (elapsed, name, arg_str, result))
+        return result
+    return clocked
+
+if __name__ == '__main__':
+    @clock
+    def sleep(sec):
+        time.sleep(sec)
+    sleep(sec=.123)
+    sleep(1.24)
+    
