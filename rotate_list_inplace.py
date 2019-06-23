f1474176f52dec4122a3549790a97d12a89eb951
Syntactical01 <46199814+Syntactical01@users.noreply.github.com>
2019-06-23 00:07:03 -0500

    Update rotate_list_inplace.py

--- rotate_list_inplace.py
+++ rotate_list_inplace.py
@@ -1,18 +1,16 @@
-a = ['a', 'b', 'c', 'd', 'e']
-b = ['d', 'e', 'a', 'b', 'c']
-rotate = -4
-
 def right_rotate_list(lst, rotate):
     """
-    O(n),
+    O(n) in place solution
     """
-    # The following line serves two porposes: (Assume list size n = 5)
+    # The following line serves two purposes: (Assume list size n = 5)
     # 1) A rotation of right 7 (rotate = 7) is the same as the rotation of right 2 (i.e. rotation = rotation % n --> 7 % 5 = 2)
-    # 2) A rotation of left 4 (rotate = -4) is the same as a right rotation of 2 (i.e. rotation = rotation % n --> -4 % 5 = 1)
+    # 2) A rotation of left 4 (rotate = -4) is the same as a right rotation of 1 (i.e. rotation = rotation % n --> -4 % 5 = 1)
     rotate %= len(lst) 
     for i in range(0, rotate):
         for j in range(i + rotate, len(lst) + i + 1, rotate):
             lst[i], lst[j % len(lst)] = lst[j % len(lst)], lst[i]
 
+a = ['a', 'b', 'c', 'd', 'e']
+rotate = -4
 right_rotate_list(a, rotate)
 print(a)
